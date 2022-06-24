92cb75231f50775ca4e1c648715102f78392558a
Roryon Wannapraserd <105912875+oporekungthai@users.noreply.github.com>
2022-06-24 16:02:39 +0700

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,2 +1,4 @@
+print("_______________________________________________________________\n")
 birthdate = int(input("Please Input your birthday : "));
 print("So your birthdate is",birthdate);
+print("_______________________________________________________________")
